3122d03780e5335c81a5c6ef657b11101913588f
combatfigh <krishnashrotriya2227@gmail.com>
2025-06-24 09:34:43 +0530

    Create ThemeContexts.tsx

---
+++ ThemeContexts.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { Appearance } from 'react-native';
+
+
+
+type Theme = 'light' | 'dark';
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType>({
+  theme: 'light',
+  toggleTheme: () => {},
+});
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  useEffect(() => {
+    const systemScheme = Appearance.getColorScheme();
+    setTheme(systemScheme || 'light');
+  }, []);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
